d2e79c46b85adb0d27767c833e46ea3f45fdcaa3
NikolaV888 <130514182+NikolaV888@users.noreply.github.com>
2023-04-12 00:58:08 -0400

    Diablaso. Ok we're back

--- Assets/Scripts/PlayerPunch.cs
+++ Assets/Scripts/PlayerPunch.cs
@@ -56,23 +56,16 @@ public class PlayerPunch : MonoBehaviour
         //Update punch collider based on the player's direction
         UpdatePunchCollider(leftPunchOrigin);
 
-        //Detect enemies in range of attack
+        //Detect enemimes in range of attack
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(leftPunchOrigin.position, punchRange, enemiesLayers);
-        Debug.Log("Number of enemies in range: " + hitEnemies.Length); // Debug line
 
         //damage enemy
         foreach (Collider2D enemy in hitEnemies)
         {
-            Vector2 enemyDirection = (enemy.transform.position - transform.position).normalized;
-            float dotProduct = Vector2.Dot(playerMovement.direction, enemyDirection);
-            Debug.Log("Dot product: " + dotProduct); // Debug line
-
-            if (dotProduct > 0)
-            {
-                enemy.GetComponent<NPCAttributes>().TakeDamage(punchDamage);
-                Debug.Log("NPC hit" + enemy.name);
-            }
+            enemy.GetComponent<NPCAttributes>().TakeDamage(punchDamage);
+            Debug.Log("NPC hit" + enemy.name);
         }
+
         //stop movement when punching
         StartCoroutine(DisableMovementDuringPunch(0.3f)); // Adjust the delay based on your punch animation duration
     }
@@ -86,71 +79,65 @@ public class PlayerPunch : MonoBehaviour
         //Update punch collider based on the player's direction
         UpdatePunchCollider(rightPunchOrigin);
 
-        //Detect enemies in range of attack
+        //Detect enemimes in range of attack
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(rightPunchOrigin.position, punchRange, enemiesLayers);
-        Debug.Log("Number of enemies in range: " + hitEnemies.Length); // Debug line
 
         //damage enemy
         foreach (Collider2D enemy in hitEnemies)
         {
-            Vector2 enemyDirection = (enemy.transform.position - transform.position).normalized;
-            float dotProduct = Vector2.Dot(playerMovement.direction, enemyDirection);
-            Debug.Log("Dot product: " + dotProduct); // Debug line
-
-            if (dotProduct > 0)
-            {
-                enemy.GetComponent<NPCAttributes>().TakeDamage(punchDamage);
-                Debug.Log("NPC hit" + enemy.name);
-            }
+            enemy.GetComponent<NPCAttributes>().TakeDamage(punchDamage);
+            Debug.Log("NPC hit" + enemy.name);
         }
+
         //stop movement when punching
-        StartCoroutine(DisableMovementDuringPunch(0.3f)); // Adjust the delay
+        StartCoroutine(DisableMovementDuringPunch(0.3f)); // Adjust the delay based on your punch animation duration
     }
-        //Proc for the movement stop
-        IEnumerator DisableMovementDuringPunch(float delay)
+
+    //Proc for the movement stop
+    IEnumerator DisableMovementDuringPunch(float delay)
+    {
+        Movement movement = GetComponent<Movement>();
+        movement.canMove = false;
+        yield return new WaitForSeconds(delay);
+        movement.canMove = true;
+    }
+
+    void UpdatePunchCollider(Transform punchOrigin)
+    {
+        BoxCollider2D punchCollider = punchOrigin.GetComponent<BoxCollider2D>();
+
+        if (playerMovement.direction == Vector2.up)
         {
-            Movement movement = GetComponent<Movement>();
-            movement.canMove = false;
-            yield return new WaitForSeconds(delay);
-            movement.canMove = true;
+            punchCollider.offset = new Vector2(0, 0.5f);
+            punchCollider.size = new Vector2(1f, 1f);
         }
-
-        void UpdatePunchCollider(Transform punchOrigin)
+        else if (playerMovement.direction == Vector2.down)
         {
-            BoxCollider2D punchCollider = punchOrigin.GetComponent<BoxCollider2D>();
-
-            if (playerMovement.direction == Vector2.up)
-            {
-                punchCollider.offset = new Vector2(0, 0.5f);
-                punchCollider.size = new Vector2(1f, 1f);
-            }
-            else if (playerMovement.direction == Vector2.down)
-            {
-                punchCollider.offset = new Vector2(0, -0.5f);
-                punchCollider.size = new Vector2(1f, 1f);
-            }
-            else if (playerMovement.direction == Vector2.left)
-            {
-                punchCollider.offset = new Vector2(-0.5f, 0);
-                punchCollider.size = new Vector2(1f, 1f);
-            }
-            else if (playerMovement.direction == Vector2.right)
-            {
-                punchCollider.offset = new Vector2(0.5f, 0);
-                punchCollider.size = new Vector2(1f, 1f);
-            }
+            punchCollider.offset = new Vector2(0, -0.5f);
+            punchCollider.size = new Vector2(1f, 1f);
         }
-
-        void OnDrawGizmosSelected()
+        else if (playerMovement.direction == Vector2.left)
         {
-            if (leftPunchOrigin != null)
-            {
-                Gizmos.DrawWireSphere(leftPunchOrigin.position, punchRange);
-            }
-
-            if (rightPunchOrigin != null)
-            {
-                Gizmos.DrawWireSphere(rightPunchOrigin.position, punchRange);
-            }
+            punchCollider.offset = new Vector2(-0.5f, 0);
+            punchCollider.size = new Vector2(1f, 1f);
+        }
+        else if (playerMovement.direction == Vector2.right)
+        {
+            punchCollider.offset = new Vector2(0.5f, 0);
+            punchCollider.size = new Vector2(1f, 1f);
         }
     }
+
+    void OnDrawGizmosSelected()
+    {
+        if (leftPunchOrigin != null)
+        {
+            Gizmos.DrawWireSphere(leftPunchOrigin.position, punchRange);
+        }
+
+        if (rightPunchOrigin != null)
+        {
+            Gizmos.DrawWireSphere(rightPunchOrigin.position, punchRange);
+        }
+    }
+}
